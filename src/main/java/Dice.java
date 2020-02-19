7177da9c8ce3020f24f18325963b3de154f4307f
Jeremy McCray <jmccray@zipcodes-MacBook-Pro-2.local>
2020-02-19 17:05:40 -0500

    gotta go get a cut

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,19 @@
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+    Integer numOfDice;
+    public Dice(Integer numOfDice){
+        this.numOfDice=numOfDice;
+    }
+
+    public Integer tossAndSum(){
+        Random random = new Random();
+        Integer sumOfDice = 0;
+        for (int i = 0; i <= numOfDice ; i++) {
+            sumOfDice+=random.nextInt(6)+1;
+        }
+
+        return sumOfDice;
+    }
 
 }
---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiceTest {
+    @Test
+
+    public void diceTest(){
+        //Given
+
+        //When
+
+        //Then
+
+
+        Assert.assertEquals(1,2);
+    }
+}
